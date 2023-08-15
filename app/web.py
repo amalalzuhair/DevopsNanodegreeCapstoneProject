ade220666fbd89aff48b7ab796f5dcbced295b09
Amal <amal.alzuhair7@gmail.com>
2023-08-15 21:00:06 +0300

    fix

--- app/web.py
+++ app/web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Udacity nanodegree app!
+    return 'Udacity nanodegree app!"
 
 app.debug = True
 app.run(host='0.0.0.0', port=80)
