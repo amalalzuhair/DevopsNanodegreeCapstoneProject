a956bda55693acdfa3023778f438266affd65f91
Amal <amal.alzuhair7@gmail.com>
2023-08-15 14:12:35 +0300

    unsucesseful lint

--- app/web.py
+++ app/web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Udacity nanodegree app!'
+    return 'Udacity nanodegree app!'d
 
 app.debug = True
 app.run(host='0.0.0.0', port=80)
