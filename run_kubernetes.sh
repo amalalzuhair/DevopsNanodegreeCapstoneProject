bd84d7568751b73649adee053beee412f7a5e81b
Amal <amal.alzuhair7@gmail.com>
2023-08-15 20:21:27 +0300

    deploy docker images to cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,16 +3,16 @@
 ## Complete the following steps to get kubernetes pods running locally
 
 # This is your Docker ID/path
-dockerpath=amalsz/capstone-project
+dockerpath="amalsz/capstone-project:v1.0.0"
 
 # Run the Docker Hub container with kubernetes
-kubectl run amalsz/capstone-project\
+kubectl run capstone-project\
     --generator=run-pod/v1\
     --image=$dockerpath\
-    --port=80 --labels amalsz/capstone-project
+    --port=80 --labels capstone-project
 
 # List kubernetes pods
 kubectl get pod
 
 # Forward the container port to a host
-kubectl port-forward amalsz/capstone-project 8000:80
+kubectl port-forward capstone-project 8000:80
